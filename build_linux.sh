45de240cdfcd3efb997fdc656b3e0eed491e5ae9
Julian Waller <git@julusian.co.uk>
2018-01-26 22:40:21 +0000

    Initial commit

---
+++ build_linux.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+set -e # stop on error
+
+cd CasparCG-Server
+
+if [ "$PLATFORM" == "legacy" ]; then
+  # TODO - combine it so that the upload steps below are shared
+  exit 0
+fi
+
+# check if build is supported
+if [ ! -d "build-scripts/${PLATFORM}" ]; then
+  echo "Build type not supported"
+  exit 0
+fi
+
+BUCKET_ID=$(echo $ORIG_COMMIT_REF | sed -r 's/[^a-z0-9]+/-/gi')
+
+# TODO - remove/change the url if it isnt going to be deployed
+curl -XPOST -H 'Accept: application/vnd.github.v3+json' -H 'Authorization: token ${GITHUB_OAUTH}' -d '{
+  "state": "pending",
+  "target_url": "https://caspar.julusian.co.uk/'$BUCKET_ID'",
+  "description": "Running build '$PLATFORM'",
+  "context": "travis-custom/'$PLATFORM'"
+}' "https://api.github.com/repos/${ORIG_REPO_NAME}/statuses/${ORIG_COMMIT_ID}"
+
+
+./build-scripts/$PLATFORM/build-docker-image
+./build-scripts/$PLATFORM/launch-interactive cmake /source && make -j2 && /source/build-scripts/$PLATFORM/package
+
+# secrets are only defined on non-pr builds, so dont try to upload if it is a pr
+# not relevent via the api hook method though
+# if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  COUNT=$(git rev-list --count HEAD)
+  NEW_NAME="CasparCG_Server_${GITHUB_COMMIT_ID}_${COUNT}_${PLATFORM}.tar.gz"
+
+  cd build/products
+  mv "CasparCG_Server_${PLATFORM}.tar.gz" "$NEW_NAME"
+  mc -C ./ config host add minio $S3_URL $S3_ACCESS_KEY $S3_SECRET_KEY S3v4
+  mc -C ./ mb minio/$BUCKET_ID
+  mc -C ./ cp "$NEW_NAME" minio/$BUCKET_ID
+# fi
+
+# TODO - remove/change the url if it isnt going to be deployed
+curl -XPOST -H 'Accept: application/vnd.github.v3+json' -H 'Authorization: token ${GITHUB_OAUTH}' -d '{
+  "state": "success",
+  "target_url": "https://caspar.julusian.co.uk/'$BUCKET_ID'",
+  "description": "Build complete '$PLATFORM'",
+  "context": "travis-custom/'$PLATFORM'"
+}' "https://api.github.com/repos/${ORIG_REPO_NAME}/statuses/${GITHUB_COMMIT_ID}"
---
+++ clone_linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e # stop on error
+
+rmdir CasparCG-Server || true
+
+# clone the repo
+mkdir CasparCG-Server
+cd CasparCG-Server
+
+git init
+git remote add origin $ORIG_REPO_URL # https://github.com/CasparCG/Server.git
+git fetch origin $ORIG_BRANCH # refs/heads/2.2.0
+git checkout $ORIG_COMMIT_ID # f613d9a79031136dc7f58ff7a946344c05ea1c53
